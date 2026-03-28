1e91f8ca25c98c8e61ff3832624a083781b5b2eb
Mohamed Bedda <bedda.wm@gmail.com>
2026-03-28 04:28:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,8 @@ def get_leaderboard():
         "success": True,
         "leaderboard": leaderboard
     })
-
+    
+init_db()
 if __name__ == "__main__":
     init_db()
     app.run(host="0.0.0.0", port=5050, debug=True)
